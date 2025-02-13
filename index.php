e6528ffc0f59a2b7c697e3c7e430e21f67aa87ed
arken <farxadov03@gmail.com>
2025-02-13 11:53:21 +0500

    koks

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 // === Настройки ===
-$googleDriveFileId = '1yq6P543McN0ONA-HhgxGbS-KIe4RvSZNryJd6XLGhpM'; // ID файла на Google Диске
+$googleDriveFileId = '1poS_CPyX5vOgpTr6M9Hkt9yOeVZVG96v'; // ID файла на Google Диске
 $jsonFile = 'data.json'; // Файл для сохранения
 $cells = [
     'month' => 'A1',
@@ -42,7 +42,7 @@ $cells = [
     'number31' => ['B32', 'C32', 'D32', 'E32', 'F32', 'G32', 'H32', 'I32', 'J32', 'K32', 'L32', 'M32', 'N32', 'O32', 'P32', 'Q32', 'R32', 'S32', 'T32', 'U32', 'V32', 'W32', 'X32', 'Y32', 'Z32', 'AA32', 'AB32', 'AC32', 'AD32', 'AE32', 'AF32', 'AG32'],
     // Добавь остальные ячейки, если нужно
 ];
-function getExcelData($googleDriveFileId, $cells, $sheetIndex = 0)
+function getExcelData($googleDriveFileId, $cells, $sheetIndex = 1)
 {
     $url = "https://drive.google.com/uc?export=download&id=$googleDriveFileId";
     $filePath = 'temp.xlsx';
